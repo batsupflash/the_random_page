e57fbf9cab9d8de9e3ff7f8059f7c295a1d8cd70
BATLAPTOP\unkno <unknow0912@outlook.com>
2017-11-05 12:56:49 +0700

    newest

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import *
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -7,8 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
-
-
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run( debug=True)
