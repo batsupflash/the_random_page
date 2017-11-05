a3e03cb3d6a26d5c57c35e890928e6f10e4e9d40
DESKTOP-CAIB5SO\Phan Thai Nam <phanthainam1092002@gmail.com>
2017-11-05 15:09:18 +0700

    First commit

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/truerandomness')
+def trueandomness():
+    return render_template('truerandomness.html')
 
 
 
