02a6ad3db6cd66665208e228e5fe952e10dddf50
ndt_912 <30501342+batsupflash@users.noreply.github.com>
2018-10-27 16:04:54 +0700

    @@

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-@app.route('/truerandomness')
-def trueandomness():
-    return render_template('truerandomness.html')
+
 
 if __name__ == '__main__':
   app.run( debug=True)
